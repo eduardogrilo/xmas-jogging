92e157e9d122431304154a5da748b44cf847f3e2
koldramke <jose_egrl@hotmail.com>
2022-04-05 09:43:50 +0100

    Almost Finished. Needs comments and final checking

--- XmasBoloReiJogging.java
+++ XmasBoloReiJogging.java
@@ -1,39 +1,32 @@
-import java.math.*;
+import java.util.Arrays;
 
 public class XmasBoloReiJogging {
-    
+
     public static void reverse(int[] array) {
-               
-        int i = 0, j = array.length - 1, tmp=0;
-        while (j > i) {
-            tmp = array[j];
-            array[j] = array[i];
-            array[i] = tmp;
-            j--;
-            i++;
-        }
-}
+        Arrays.sort(array);
+    }
+
     public static long getMinimumDistance(int[] calories) {
-        
+
         reverse(calories);
         long sum = 0;
 
-        if (calories.length < 1 || calories.length > 40){
+        if (calories.length < 1 || calories.length > 40) {
             return -1;
         }
 
-
-        for (int index = 0; index < calories.length; index++){
-            if (calories[index] <1 || calories [index] > 1000) {
+        for (int index = 0; index < calories.length; index++) {
+            if (calories[index] < 1 || calories[index] > 1000) {
                 return -1;
             }
             sum += (long) Math.pow(2, index) * calories[index];
         }
-        return(sum);
+        return (sum);
     }
+
     public static void main(String[] args) {
-        //int calories[] = {5 , 7 , 10};
-        int calories[] = {56,4};
+        int calories[] = {5, 10, 7};
+        // int calories[] = {56,4};
         System.out.println(getMinimumDistance(calories));
     }
 }
