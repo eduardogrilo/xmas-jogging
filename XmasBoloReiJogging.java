a7b3ab730de4108a12d5545fb9dd768f2277156e
koldramke <jose_egrl@hotmail.com>
2022-04-05 10:45:56 +0100

    Job done!

--- XmasBoloReiJogging.java
+++ XmasBoloReiJogging.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 
 public class XmasBoloReiJogging {
 
+    // Change order from highest value to to lowest value (reverse order)
     public static void reverse(Integer[] array) {
         Arrays.sort(array, Collections.reverseOrder());
     }
@@ -10,6 +11,10 @@ public class XmasBoloReiJogging {
     public static long getMinimumDistance(Integer[] calories) {
 
         long sum = 0;
+
+        // Reverse order allows highst values to multiplicate 2^0, 2^1...
+        // which decreases the final result (sum) to the lowest value possible
+        // (that's the goal of the task)
         reverse(calories);
 
         if (calories.length < 1 || calories.length > 40) {
@@ -20,11 +25,13 @@ public class XmasBoloReiJogging {
             if (calories[index] < 1 || calories[index] > 1000) {
                 return -1;
             }
+            // Main formula of the task
             sum += (long) Math.pow(2, index) * calories[index];
         }
         return (sum);
     }
 
+    // Driver Code
     public static void main(String[] args) {
         Integer calories[] = new Integer[] { 5, 10, 7 };
         // Integer calories[] = new Integer[] {56, 45, 1, 565, 23};
